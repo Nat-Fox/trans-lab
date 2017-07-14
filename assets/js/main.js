27033e7e800c964dfacc11f0bf6c216798b7b704
Natalia Garrido <natgramirez@outlook.com>
2017-07-13 20:45:11 -0400

    agregando vistas de saldo, calculos y preguntas frecuentes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,14 @@
 /* Holiii acá va tu código también :D*/
 $(document).ready(function() {
-    // Initialize collapse button
-    $(".button-collapse").sideNav();
-    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-    //$('.collapsible').collapsible();
-
-
+    // Para el funcionamiento del sideNav
+    $('.button-collapse').sideNav();
+    // Para el funcionamiento del select
+    $('select').material_select();
 
 });
 
 // FALTA REMOVER LOS MENSAJES DE ERROR
+
 
 //función para validar el correo
 function validarCorreo(correo) {
@@ -35,36 +34,64 @@ function contenidoAlfa(data) {
     }
 }
 
-var btnRegis = $('#btn-ini').on('click', function() {
+$('#btn-ini').on('click', function() {
+
     // Valores ingresados por el usuario
     var correo = $('#input-email').val();
+    console.log('correo', correo);
     var pass = $('#pass').val();
     // Validaciones para el correo
     if (correo !== '') {
         if (validarCorreo(correo)) {
-            $('#input-email').parent().append('<span>Ingresa un correo valido. Ejem: name@domain.com</span>');
+            $('#error-correo').parent().append('<div>Ingresa un correo valido. Ejem: name@domain.com</div>');
         }
     } else {
-        $('#input-email').parent().append('<span>Debes ingresar tu correo</span>');
+        $('#error-correo').parent().append('<div>Debes ingresar tu correo</div>');
     }
 
 
     // Validaciones para la contraseña
     if (pass !== '') {
         if (password(pass)) {
-            $('#pass').parent().append('<span>Debes ingresar una contraseña más segura.</span>');
+            $('#error-clave').parent().append('<div>Debes ingresar una contraseña más segura.</div>');
         }
         if (!contenidoAlfa(pass)) {
-            $('#pass').parent().append('<span>Solo se permiten números.</span>');
+            $('#error-clave').parent().append('<div>Solo se permiten números.</div>');
         }
     } else {
-        $('#pass').parent().append('<span>Debes ingresar tu contraseña</span>');
+        $('#error-clave').parent().append('<div>Debes ingresar tu contraseña</div>');
     }
 
     // Si todo esta correcto se limpia el formulario
     if (contenidoAlfa(pass) && !validarCorreo(correo)) {
         console.log('correcto');
-        $(location).attr('href', 'menu.html')
+        // Se guarda el correo en local storage
+        localStorage.correo = correo;
+        $(location).attr('href', 'menu.html');
     }
 
 })
+
+
+//var email = document.getElementById('input-email').value;
+
+
+$('#btn-perfil').on('click', function() {
+    console.log('click');
+    console.log(localStorage.correo);
+    $('#correo-ingresado').append(`<a class="btn btn-ini btn-mail" id="correo-ingresado">${localStorage.correo}</a`);
+    //$(location).attr('href', 'perfil.html');
+
+
+});
+
+// var element = document.querySelector(".btn-perfil");
+// element.addEventListener('submit', function() {
+//     console.log(click);
+// });
+// $('#btn-perfil').on('click', function() {
+
+//         // var correoIngresado = $('#input-email').val();
+//         // console.log('correo', correoIngresado);
+//         // $('#correo-ingresado').append(`<a class="btn btn-ini btn-mail" id="correo-ingresado">${correo}</a`);
+// });
